7e384e0a4e08015f23a804fcc7ae605ffdda7306
yottyan-1070 <75198514+yottyan-1070@users.noreply.github.com>
2021-06-15 16:56:22 +0900

    prefix changed.

--- discordbot.py
+++ discordbot.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='ys!')
+bot = commands.Bot(command_prefix='!')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
